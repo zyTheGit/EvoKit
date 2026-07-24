5ca79636c998f2b4a2642827d77d8195c724c0d0
EvoKit <user@evokit.dev>
2026-07-24 18:12:13 +0800

    chore: 更新 Homebrew formula 至 v0.6.4

--- homebrew/Formula/evokit.rb
+++ homebrew/Formula/evokit.rb
@@ -4,8 +4,8 @@ class Evokit < Formula
   license "MIT"
   depends_on "node"
 
-  url "https://registry.npmjs.org/@zythegit/evokit/-/evokit-0.2.1.tgz"
-  sha256 "205d1980b01a26575e4180a011fe2b5040c1214fa82c611df87486e39f602555"
+  url "https://registry.npmjs.org/@zythegit/evokit/-/evokit-0.6.4.tgz"
+  sha256 "36a611f0f7a1b81ffee050a82662c489acee20efe3b5ee97f87b1b90b9f29b35"
 
   def install
     system "npm", "install", *std_npm_args(prefix: false)
